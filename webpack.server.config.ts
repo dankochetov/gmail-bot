f1778b8faa3c9798b40f506ffc9b87590758f231
Danil Kochetov <danil.kochetov@gmail.com>
2020-07-10 21:48:55 +0300

    Disable server minification

--- webpack.server.config.ts
+++ webpack.server.config.ts
@@ -1,7 +1,6 @@
 import { Configuration } from 'webpack';
 import nodeExternals from 'webpack-node-externals';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
-import TerserPlugin from 'terser-webpack-plugin';
 
 function config(options: Configuration): Configuration {
     return {
@@ -15,17 +14,7 @@ function config(options: Configuration): Configuration {
         ],
         optimization: {
             ...options.optimization,
-            minimizer: [
-                new TerserPlugin({
-                    cache: true,
-                    parallel: true,
-                    sourceMap: true,
-                    terserOptions: {
-                        // Required for TypeORM migrations
-                        keep_classnames: true,
-                    },
-                }),
-            ],
+            minimize: false,
         },
     };
 }
